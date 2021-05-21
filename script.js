e2b7d8a4d6dc855b74e62e5885a112154f27b3b7
AshCoder232 <aswanths208@gmail.com>
2021-05-21 22:41:55 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,36 @@
+//Support
+function clickAlert() {
+    alert("Your Query has been Submitted");
+}
+
+//Login Page
+function signedIn() {
+    alert("Welcome Back!");
+}
+
+function acccreated() {
+    alert("Your new account is created!");
+}
+
+function redirecthome(){
+    window.location.href = "index.html";
+}
+
+function redirectlogin(){
+    window.location.href = "login.html";
+}
+
+function redirectsignup(){
+    window.location.href = "sign_up.html";
+}
+
+function redirectcourses(){
+    window.location.href = "courses.html";
+}
+function redirectinstructors(){
+    window.location.href="instructors.html#instructor-app";
+}
+
+function redirectstudent(){
+    window.location.href="index.html#student-app";
+}
